b5ba6c558fe2d770b736c07e1c7f851104953b55
Giuliano Gomes Da Silva <gomesdasilva.giuliano@gmail.com>
2022-09-23 19:40:17 -0300

    Spring Heroku app config

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  //  @Value("${app.varexample}")
+ //   String message;
+
+    @GetMapping("/hola")
+    public String holaMundo(){
+
+ //       System.out.println(message);
+        return "Hola como estas";
+    }
+
+
+}
